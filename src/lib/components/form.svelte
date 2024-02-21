4d10aa8d87b3488a91260f11bc831f30e9d29b52
Jonas Escher <joe@tincan.ch>
2024-02-21 09:34:07 +0100

    form component

---
+++ src/lib/components/form.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	let Tally: any;
+
+	onMount(() => {
+		Tally = window.Tally;
+	});
+
+	/* interface LoadedPayload {
+		formId: string;
+	}
+
+	window.addEventListener('message', (e) => {
+		if (e?.data?.includes('Tally.FormLoaded')) {
+			const payload = JSON.parse(e.data).payload as LoadedPayload;
+			console.log('Form loaded', payload.formId);
+			// ...
+		}
+	}); */
+</script>
+
+<svelte:head>
+	<script src="https://tally.so/widgets/embed.js"></script>
+</svelte:head>
+<svelte:window on:load={Tally.loadEmbeds()} />
+
+<iframe
+	data-tally-src="https://tally.so/embed/wgM7p4?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+	loading="lazy"
+	width="100%"
+	height="621"
+	frameborder="0"
+	marginheight="0"
+	marginwidth="0"
+	title="null"
+></iframe>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,28 +1,23 @@
 <script>
 	import Animal2 from '$lib/components/animal2.svelte';
 	import Button from '$lib/components/button.svelte';
+	import Form from '$lib/components/form.svelte';
 	import Rainbow from '$lib/components/rainbow.svelte';
+
+	let showForm = false;
+
+	$: console.log('showForm', showForm);
 </script>
 
-<svelte:head>
-	<script src="https://tally.so/widgets/embed.js"></script>
-</svelte:head>
-<svelte:window on:load={Tally.loadEmbeds()} />
 <section class="container">
 	<h1 class="font-heading text-6xl">No Firlefanz without a firleplan</h1>
 </section>
 <section class="container py-12">
 	<article class="max-w-xl">
-		<iframe
-			data-tally-src="https://tally.so/embed/wgM7p4?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
-			loading="lazy"
-			width="100%"
-			height="621"
-			frameborder="0"
-			marginheight="0"
-			marginwidth="0"
-			title="null"
-		></iframe>
+		<input type="checkbox" bind:checked={showForm} class="mb-4" />
+		{#if showForm}
+			<Form />
+		{/if}
 	</article>
 	<div class="mt-12">
 		<a href="/">
